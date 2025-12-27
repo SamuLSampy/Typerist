a0bd3f1b8f02bb7a3cc8865d4dbcb5b5727e76ac
SamuLSampy <samuelbsamp@gmail.com>
2025-12-26 22:07:04 -0300

    Correção para Vercel

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const User = require('../models/user');
+const User = require('../models/User');
 const { render } = require('ejs');
 
 exports.paginaInicial = (req, res) => {
